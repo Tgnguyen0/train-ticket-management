e203eca36209f60c9f9e1129f5ae1a47053335b9
Tgnguyen0 <nguyennhattan223344@gmail.com>
2024-10-16 09:44:23 +0700

    16/10/2024 9:44 AM Tgnguyen0

--- src/main/java/app/Dao/NhanVien_DAO.java
+++ src/main/java/app/Dao/NhanVien_DAO.java
@@ -1,4 +1,117 @@
 package app.Dao;
 
 public class NhanVien_DAO {
+    /*String INSERT_SQL = "INSERT INTO NhanVien (MaNV, MatKhau, HoTen,GioiTinh, VaiTro,DiaChi,SDT,Email,Luong,Hinh) VALUES (?, ?, ?,?,?, ?,?,?,?,?)";
+    String UPDATE_SQL = "UPDATE NhanVien SET MatKhau=?, HoTen=?, GioiTinh=?,VaiTro=?,DiaChi=?,SDT=?,Email=?,Luong=?,Hinh=? WHERE MaNV=?";
+    String DELETE_SQL = "DELETE FROM NhanVien WHERE MaNV=?";
+    String SELECT_ALL_SQL = "SELECT * FROM NhanVien";
+    String SELECT_BY_ID_SQL = "select * from NhanVien where MaNV =?";
+
+    public int insert(NhanVien entity) {
+        int i;
+        try {
+            i = 1;
+            XJdbc.update(INSERT_SQL,
+                    entity.getMaNV(),
+                    entity.getMatKhau(),
+                    entity.getHoTen(),
+                    entity.isGioiTinh(),
+                    entity.isVaiTro(),
+                    entity.getDiaChi(),
+                    entity.getsDT(),
+                    entity.getEmail(),
+                    entity.getLuong(),
+                    entity.getHinh()
+            );
+        } catch (Exception e) {
+            i = 0;
+        }
+        return i;
+    }
+
+    public int update(NhanVien entity) {
+        int i;
+        try {
+            i = 1;
+            XJdbc.update(UPDATE_SQL,
+                    entity.getMatKhau(),
+                    entity.getHoTen(),
+                    entity.isGioiTinh(),
+                    entity.isVaiTro(),
+                    entity.getDiaChi(),
+                    entity.getsDT(),
+                    entity.getEmail(),
+                    entity.getLuong(),
+                    entity.getHinh(),
+                    entity.getMaNV()
+            );
+        } catch (Exception e) {
+            i = 0;
+        }
+        return i;
+    }
+
+    public int delete(String id) {
+        int i;
+        try {
+            i = 1;
+            XJdbc.update(DELETE_SQL,id);
+        } catch (Exception e) {
+            i = 0;
+        }
+        return i;
+
+    }
+
+    public NhanVien selectById(String manv) {
+        List<NhanVien> list = this.selectBySql(SELECT_BY_ID_SQL, manv);
+        return list.size() > 0 ? list.get(0) : null;
+    }
+
+    public List<NhanVien> selectAll() {
+        return this.selectBySql(SELECT_ALL_SQL);
+    }
+
+    protected List<NhanVien> selectBySql(String sql, Object... args) {
+        List<NhanVien> list = new ArrayList<>();
+        try {
+            ResultSet rs = null;
+            try {
+                rs = XJdbc.query(sql, args);
+                while (rs.next()) {
+                    NhanVien entity = new NhanVien();
+                    entity.setMaNV(rs.getString("MaNV"));
+                    entity.setMatKhau(rs.getString("MatKhau"));
+                    entity.setHoTen(rs.getString("HoTen"));
+                    entity.setGioiTinh(rs.getBoolean("GioiTinh"));
+                    entity.setVaiTro(rs.getBoolean("VaiTro"));
+                    entity.setDiaChi(rs.getString("DiaChi"));
+                    entity.setsDT(rs.getString("SDT"));
+                    entity.setEmail(rs.getString("Email"));
+                    entity.setLuong(rs.getDouble("Luong"));
+                    entity.setHinh(rs.getString("hinh"));
+                    list.add(entity);
+                }
+            } finally {
+                rs.getStatement().getConnection().close();
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+            throw new RuntimeException(ex);
+        }
+        return list;
+    }
+
+    public List<NhanVien> selectByKeyword(String keyword) {
+        String sql = "SELECT * FROM NhanVien WHERE HoTen LIKE ?";
+        return this.selectBySql(sql, "%" + keyword + "%");
+    }
+    public void testLogin(String username, String password){
+
+        if(username.equals("Duy") && password.equals("anhduy2002")){
+
+        }else{
+
+        }
+    }*/
 }
--- src/main/java/app/Dao/Ve_DAO.java
+++ src/main/java/app/Dao/Ve_DAO.java
@@ -1,18 +1,20 @@
 package app.dao;
 
-import app.thuc_the.Ghe;
+import app.ket_noi_co_so_du_lieu.KetNoiCoSoDuLieu;
+import app.thuc_the.DaiNgo;
 import app.thuc_the.Ve;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Ve_DAO {
     // Các câu lệnh SQL
-    String INSERT_SQL = "INSERT INTO SanPham (MaSanPham, TenSanPham, DonGia, Size, Hinh, MaLoai) VALUES (?,?,?,?,?,?)";
-    String UPDATE_SQL = "UPDATE SanPham SET TenSanPham=?, DonGia=?, Size=?, Hinh=?, MaLoai=? WHERE MaSanPham=?";
-    String DELETE_SQL = "DELETE FROM SanPham WHERE MaSanPham=?";
-    String SELECT_ALL_SQL = "SELECT * FROM SanPham";
-    String SELECT_BY_ID_SQL = "SELECT * FROM SanPham WHERE MaSanPham =?";
+    String NHAP_SQL = "INSERT INTO KhachHang (MaKH, TenKH, DiaChi, SoDT, Email, GioiTinh) values (?, ?, ?, ?, ?, ?)";
+    String CAP_NHAT_SQL = "UPDATE TenKH=?, DiaChi=?, SoDT=?, Email=?, GioiTinh=? WHERE MaKH=?";
+    String CHON_TAT_SQL = "SELECT * FROM KhachHang";
+    String CHON_THEO_MA_SQL = "SELECT * FROM KhachHang WHERE MaKH=?";
 
     List<Ve> dsVe; // Tránh thông tin bị trùng
 
@@ -65,5 +67,125 @@ public class Ve_DAO {
         veCanCapNhat.setLoaiVe(ve.getLoaiVe());
 
         return true;
+    }
+
+    // Lưu kh vào cơ sở dữ liệu
+    public int LuuKHVaoCSDL(Ve ve) {
+        int i;
+        try {
+            i = 1;
+            KetNoiCoSoDuLieu.CapNhat(
+                    NHAP_SQL,
+                    ve.getMaVe(),
+                    ve.getNgayDatVe(),
+                    ve.getGiaVe(),
+                    ve.getKhachHang().getMaKH(),
+                    ve.getDaiNgo(),
+                    ve.getGaKhoiHanh(),
+                    ve.getGaKetThuc(),
+                    ve.getGhe().getMaGhe(),
+                    ve.getLoaiVe(),
+                    ve.getLoaiDoiTuong(),
+                    ve.getNgayKhoiHanh()
+            );
+        } catch (Exception e) {
+            i = 0;
+        }
+
+        return i;
+    }
+
+    public int CapNhatKhachHangCSDL(Ve ve) {
+        int i = 0;
+
+        try {
+            i = 1;
+            KetNoiCoSoDuLieu.CapNhat(
+                    CAP_NHAT_SQL,
+                    ve.getNgayDatVe(),
+                    ve.getGiaVe(),
+                    ve.getKhachHang().getMaKH(),
+                    ve.getDaiNgo(),
+                    ve.getGaKhoiHanh(),
+                    ve.getGaKetThuc(),
+                    ve.getGhe().getMaGhe(),
+                    ve.getLoaiVe(),
+                    ve.getLoaiDoiTuong(),
+                    ve.getNgayKhoiHanh(),
+                    ve.getMaVe()
+            );
+        } catch (Exception e) {
+            i = 0;
+        }
+
+        return i;
+    }
+
+    public Ve ChonTheoID(String manv) {
+        List<Ve> list = this.ChonSQL(CHON_THEO_MA_SQL, manv);
+        return list.size() > 0 ? list.get(0) : null;
+    }
+
+    public List<Ve> ChonTatCa() {
+        return this.ChonSQL(CHON_TAT_SQL);
+    }
+
+    public List<Ve> ChonSQL(String lenhSQL, Object... thamSo) {
+        List<Ve> list = new ArrayList<>();
+        try {
+            ResultSet boKetQua = null;
+            try {
+                boKetQua = KetNoiCoSoDuLieu.TruyVan(lenhSQL, thamSo);
+                while (boKetQua.next()) {
+                    DaiNgo daiNgo = DaiNgo.GIAMGIAKHONGPHANTRAM;
+                    Ve ve = new Ve();
+
+                    if (boKetQua.getDouble("DaiNgo") == DaiNgo.GIAMGIAHAIMUOIPHANTRAM.getValue()) {
+                        daiNgo = DaiNgo.GIAMGIAHAIMUOIPHANTRAM;
+                    }
+
+                    if (boKetQua.getDouble("DaiNgo") == DaiNgo.GIAMGIANAMPHANTRAM.getValue()) {
+                        daiNgo = DaiNgo.GIAMGIANAMPHANTRAM;
+                    }
+
+                    if (boKetQua.getDouble("DaiNgo") == DaiNgo.GIAMGIAMUOIPHANTRAM.getValue()) {
+                        daiNgo = DaiNgo.GIAMGIAMUOIPHANTRAM;
+                    }
+
+                    ve.setMaVeTuCSDL(boKetQua.getString("MaVe"));
+                    ve.setNgayDatVe(boKetQua.getDate("NgayDatVe").toLocalDate());
+                    ve.setGiaVe(boKetQua.getDouble("GiaVe"));
+                    //ve.setKhachHang(boKetQua.getString("MaKH"));
+                    ve.setDaiNgo(daiNgo);
+                    ve.setGaKetThuc(boKetQua.getString("GaKhoiHanh"));
+                    ve.setGaKhoiHanh(boKetQua.getString("GaKetThuc"));
+                    //ve.setGhe(boKetQua.getString("MaGhe"));
+                    ve.setLoaiVe(boKetQua.getString("LoaiVe"));
+                    ve.setLoaiDoiTuong(boKetQua.getString("LoaiDoiTuong"));
+                    ve.setNgayKhoiHanh(boKetQua.getDate("NgayKhoiHanh").toLocalDate());
+                    list.add(ve);
+                }
+            } finally {
+                boKetQua.getStatement().getConnection().close();
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+            throw new RuntimeException(ex);
+        }
+        return list;
+    }
+
+    public List<Ve> ChonTheoTuKhoa(String keyword) {
+        String sql = "SELECT * FROM Ve WHERE TenKH LIKE ?";
+        return this.ChonSQL(    sql, "%" + keyword + "%");
+    }
+
+    public void KiemTraDangNhap(String nguoiDung, String matKhau){
+
+        if(nguoiDung.equals("sa") && matKhau.equals("123")){
+
+        }else{
+
+        }
     }
 }
